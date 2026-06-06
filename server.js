1a43fa7217cfc350a613603c480107877ae0af9c
Nassour Nourene Adam <todjenassournourene@gmail.com>
2026-06-07 00:26:30 +0200

    Fix Render Docker email deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
